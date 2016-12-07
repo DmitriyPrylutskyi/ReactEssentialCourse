1001141a3fe1d2ce90e4b2fac4744c701277d5e1
dmitriyprylutskyi <dprily@gmail.com>
2016-12-08 01:07:45 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+// http://webpack.github.io/docs/configuration.html
+// http://webpack.github.io/docs/webpack-dev-server.html
+var app_root = 'src_lesson11'; // the app root folder: src, src_users, etc
+var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  app_root: app_root, // the app root folder, needed by the other webpack configs
+  entry: [
+    'babel-polyfill',
+    __dirname + '/' + app_root + '/index.js',
+  ],
+  output: {
+    path: __dirname + '/public/js',
+    publicPath: 'js/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+      },
+      {
+        // https://github.com/jtangelder/sass-loader
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+     ],
+  },
+  context: path.join(__dirname, app_root),
+  plugins: [
+    new CleanWebpackPlugin(['css/main.css', 'js/bundle.js', 'img/*'], {
+      root: __dirname + '/public',
+      verbose: true,
+      dry: false, // true for simulation
+    }),
+    new CopyWebpackPlugin([{
+      from: 'img', to: '../img'
+    }], {debug: 'info'}),
+  ],
+};
